7520389e8275a60ce84260184f964e13b7ee601b
budiartoa <none@none>
2012-03-28 10:05:56 +1100

    new plugin settings for callbacks

---
+++ view/plugin_settings_section_editor_functionalities.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Generic instruction for the callback setup
+ * User: budiartoa
+ * Date: 16/03/12
+ * Time: 11:45 AM
+ * @copyright Copyright © Luxbet Pty Ltd. All rights reserved. http://www.luxbet.com/
+ * @license http://www.opensource.org/licenses/BSD-3-Clause
+ */
+?>
+
+<p>
+	Make it static alse features some enhancements to the editor
+	<ol>
+		<li>
+			<strong>Ability to inject raw html codes such as scripts <span style="color:red">Use with caution</span></strong><br />
+			Putting any code between shortcode <strong>[static_code] [/static_code]</strong> will cause the section to be saved unescaped hence custom css and js are possible
+		</li>
+	</ol>
+</p>
